f5f5a562cc192628cd83d47f89192ec20cfc0fb8
pemudakoding <pemudakoding@gmail.com>
2024-10-29 22:12:07 +0800

    fix(seeder): conflict namespace at PermissionsSeeder

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Enums\Permission;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Permission as PermissionModel;
 use Spatie\Permission\PermissionRegistrar;
 
 class PermissionsSeeder extends Seeder
@@ -21,7 +21,7 @@ class PermissionsSeeder extends Seeder
 
         $permissions = Permission::mapPermissions();
 
-        Permission::query()->upsert(
+        PermissionModel::query()->upsert(
             array_merge(...array_values($permissions)),
             'name',
             ['name', 'guard_name']
