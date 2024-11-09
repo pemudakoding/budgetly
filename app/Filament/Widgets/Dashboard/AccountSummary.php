a6dafab6efdfd42dfa253e6deb67f6fe0e195e17
holiq <holiq.ibrahim376@gmail.com>
2024-11-09 11:14:00 +0700

    feat(dashboard): widget account summary

--- app/Filament/Widgets/Dashboard/AccountSummary.php
+++ app/Filament/Widgets/Dashboard/AccountSummary.php
@@ -2,39 +2,58 @@
 
 namespace App\Filament\Widgets\Dashboard;
 
+use App\Enums\Period;
+use App\Models\Account;
+use App\Models\IncomeBudget;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
 
 class AccountSummary extends ApexChartWidget
 {
-    /**
-     * Chart Id
-     */
+    use InteractsWithPageFilters;
+
     protected static ?string $chartId = 'AccountSummary';
 
-    /**
-     * Widget Title
-     */
-    protected static ?string $heading = 'AccountSummary';
+    protected static ?string $heading = 'Account Summary';
+
+    protected int|string|array $columnSpan = 'full';
 
     /**
-     * Chart options (series, labels, types, size, animations...)
-     * https://apexcharts.com/docs/options
+     * @return array<string, mixed>
      */
     protected function getOptions(): array
     {
+        if ($this->filters['period'] === Period::Custom->value) {
+            $startDate = $this->filters['startDate'] ?? now();
+            $endDate = $this->filters['endDate'] ?? now();
+        } else {
+            /** @var array<int, mixed> $period */
+            $period = Period::getDateFrom(Period::tryFrom($this->filters['period']));
+
+            [$startDate, $endDate] = $period;
+        }
+
+        $accounts = Account::query()->where('user_id', auth()->id())->get();
+        $balances = IncomeBudget::query()->whereBelongsToUser(auth()->user())
+            ->whereBetween('created_at', [$startDate, $endDate])
+            ->with('income.account')->get()
+            ->groupBy('income.account.name')
+            ->map(fn ($income) => $income->sum('amount'))
+            ->values();
+
         return [
             'chart' => [
                 'type' => 'bar',
-                'height' => 300,
+                'height' => 150,
             ],
             'series' => [
                 [
                     'name' => 'BasicBarChart',
-                    'data' => [7, 10, 13, 15, 18],
+                    'data' => $balances,
                 ],
             ],
             'xaxis' => [
-                'categories' => ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+                'categories' => $accounts->pluck('name')->toArray(),
                 'labels' => [
                     'style' => [
                         'fontFamily' => 'inherit',
@@ -48,10 +67,10 @@ class AccountSummary extends ApexChartWidget
                     ],
                 ],
             ],
-            'colors' => ['#f59e0b'],
+            'colors' => $accounts->pluck('legend')->toArray(),
             'plotOptions' => [
                 'bar' => [
-                    'borderRadius' => 3,
+                    'borderRadius' => 2,
                     'horizontal' => true,
                 ],
             ],
